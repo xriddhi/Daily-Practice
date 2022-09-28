28e92597904e7c8bc543ea7afb3716761d41cf25
Riddhi S <114600109+xriddhi@users.noreply.github.com>
2022-09-28 21:02:10 +0530

    Add files via upload

---
+++ Practice28Sep22.java
@@ -0,0 +1,53 @@
+//28.Sep.2022 
+import java.util.Scanner;
+
+import javax.sound.midi.SoundbankResource;
+import javax.swing.plaf.synth.SynthPasswordFieldUI;
+public class Project {
+    public static void main(String[]args){
+        //Q.1 Find Percentage!
+
+        //A.1
+        //Scanner scanner = new Scanner(System.in)
+           // System.out.println("Enter Subject One Marks:");
+           // Float subOne = scanner.nextFloat();
+            //System.out.println("Enter Subject Two Marks:");
+           // Float subTwo = scanner.nextFloat();
+           // Float Sum = subOne + subTwo;
+            //Float Perc = Sum/200*100;
+           // System.out.println(Perc);
+
+            //Q.2 Print Name
+            //A.2
+           // System.out.println("Hi!What's your name?");
+            //Scanner sc = new Scanner(System.in);
+            //String name = sc.next();
+            //System.out.println("Hey"+" "+ name +",have an aww-some day!");
+
+            //Q.3 Convert Kilometeres into Miles
+            //A.3
+           // Scanner kilo = new Scanner(System.in);
+            //System.out.println("How many Kilo-meters is it?");
+            //Double howMuchKM = kilo.nextDouble();
+           // Double howMuchMile = howMuchKM*0.621371;
+            //System.out.println("Hey,it is "+ howMuchMile);
+            
+            //Q.4 Is it an Integer?
+            //A.4 
+            //Scanner sc = new Scanner(System.in);
+           // System.out.println("Enter A Number.Is it an Integer? Oh,you cannot tell? Let me help!");
+            
+            //System.out.println(sc.hasNextInt());
+
+            
+
+
+            
+
+
+
+        }
+        
+
+
+    }
