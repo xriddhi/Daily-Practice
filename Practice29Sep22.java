e9a76ba4d56217e9ae77c424e752aee0c2f9484e
Riddhi S <114600109+xriddhi@users.noreply.github.com>
2022-09-29 22:58:24 +0530

    Add files via upload

---
+++ Practice29Sep22.java
@@ -0,0 +1,26 @@
+public class Practice29Sep22 {
+    public static void main(String[] args) {
+        //Topic 1. Precedance And Associativity
+        // * and /have same precedance
+        
+        //then we hop to Associativity FROM LEFT TO RIGHT
+        // + and - have same prec = 11
+        // * and % and / have prec of 12
+
+
+        //Topic 2. Data types of expressions 
+        // b+s=i
+        //s+i=i
+        //L+f=f
+        //i+f=f
+        //c+i=i
+        //c+s=i
+        //L+d=d
+        //f+d=d
+        // b=byte,c=char,d=double,l=long,s=short,i=int,f=float
+        
+        
+
+
+    }
+}
