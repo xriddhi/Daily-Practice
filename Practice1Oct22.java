2bac3efc6540765bbb1e9db7a5c86ef5b4720024
Riddhi S <114600109+xriddhi@users.noreply.github.com>
2022-10-01 20:13:17 +0530

    Add files via upload

---
+++ Practice1Oct22.java
@@ -0,0 +1,18 @@
+
+public class Practice1Oct22 {
+    public static void main(String[] args) {
+        //%d = int
+        //%s=string
+        //%f=float
+        //%c=char
+        
+        
+        int a = 16;
+        float b = 17.098783f;
+
+        System.out.printf("The integer is %d and the Float is %f", a, b);
+        System.out.format("The integer is %d and the Float is %f", a, b);
+
+
+    }
+}
