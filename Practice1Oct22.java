a48d30c265925643bd087ceba010794f546996a9
Riddhi S <114600109+xriddhi@users.noreply.github.com>
2022-10-01 22:38:30 +0530

    Update Practice1Oct22.java

--- Practice1Oct22.java
+++ Practice1Oct22.java
@@ -1,17 +1,16 @@
 
 public class Practice1Oct22 {
     public static void main(String[] args) {
+        
+        //1.String is a class but it can be used as a Primitive Data type.
+ 
         //%d = int
         //%s=string
         //%f=float
         //%c=char
         
         
-        int a = 16;
-        float b = 17.098783f;
-
-        System.out.printf("The integer is %d and the Float is %f", a, b);
-        System.out.format("The integer is %d and the Float is %f", a, b);
+      
 
 
     }
