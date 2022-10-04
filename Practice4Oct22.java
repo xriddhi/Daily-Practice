11bdcd331bc41e333f50600ce73c4a6131db41f5
Riddhi S <114600109+xriddhi@users.noreply.github.com>
2022-10-04 17:32:36 +0530

    Update Practice4Oct22.java

--- Practice4Oct22.java
+++ Practice4Oct22.java
@@ -1,6 +1,39 @@
 public class Practice4Oct22 {
     public static void main(String[] args){
         //Escape sequence characters
+        /n //next line
+            /t 
+            //new tab
+            "//" 
+            //for /
+            /'  
+            //for ' Single quote
+            /" 
+            //for double quote
+            /b 
+            //for backspace
+            
+            
+            //2. Converting string to Lower Case
+            String name = "RiDdhi";
+        System.out.println(name.toLowerCase());
+        
+        
+        //3.Replace strings
+       
+        String text = "I am Riddhi.";
+        String textR = text.replace("Riddhi","Anshul");
+        System.out.println(textR);
+        
+        
+        //4. 
+        String text = "Dear <|name|> Thanks a lot!";
+          text = text.replace("<|name|>","riddhi");
+  
+  
+         System.out.println(text);
+        
+        
         
     }
 }
