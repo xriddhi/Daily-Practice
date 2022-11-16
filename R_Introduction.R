1d5e91d39feb531a6a05cc964f66d2a750c4b2b3
Riddhi S <114600109+xriddhi@users.noreply.github.com>
2022-11-16 10:16:18 +0530

    Update R_Introduction.R

--- R_Introduction.R
+++ R_Introduction.R
@@ -1,1 +1,46 @@
-#' *welcome to HS 31: Data analysis through R*
+#' *welcome to HS 31: Data analysis through R
+print("Hello,world")
+#hello
+"Hi"
+"okay"
+8+12
+#Introduction to R
+#HS 31:Data Analytics
+#Intro To R
+#Simple Calculations
+3+4
+6+22
+5-5
+3*5
+(5+5)/2
+5+5/2
+2^4
+28%%6
+x <- 42
+y<- 40
+z<- 23
+a<- true
+a<-"true"
+x
+y
+a
+z
+my_apples <-45
+my_apples <- 42
+my_apples
+my_oranges <- 29
+my_fruits <- my_apples+my_oranges
+my_fruits
+#DATA TYPES
+my_numeric <-42
+my_character <- "riddhi"
+my_logical <- FALSE
+class(my_numeric)
+class(my_apples)
+class(my_character)
+class(my_logical)
+vegas <- "Go!"
+num_vector <- c("a","1","b")
+# c is for combining
+#c- concactination
+*
